f6d0f1690bba05afce74dd5945478ba99016afb2
Sacha Ayoun <sachaayoun@gmail.com>
2020-05-26 18:45:10 +0100

    fix depgraph call

--- compileUtils/moveFiles.sh
+++ compileUtils/moveFiles.sh
@@ -2,7 +2,7 @@ virtualenv --python=python3 venv
 source venv/bin/activate
 pip install -r compileUtils/requirements.txt
 rm -rf customlib
-find . -iname '*.ml' -or -iname '*.mli' | grep -v '_esy\|_build' | depgraph > depend.dot
+find . -iname '*.ml' -or -iname '*.mli' | grep -v '_esy\|_build' | esy depgraph > depend.dot
 mkdir customlib
 cp compileUtils/dune_for_compcert customlib/dune
 cp compcert.ini customlib/compcert.ini
